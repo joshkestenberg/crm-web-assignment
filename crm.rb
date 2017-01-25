147b09d307900b1460fcbbcc90a3d5258cf62631
joshkestenberg <joshkestenberg@gmail.com>
2017-01-25 18:12:48 -0500

    edit function works

--- crm.rb
+++ crm.rb
@@ -16,6 +16,14 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/about' do
+  erb :about
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
   if @contact
@@ -25,15 +33,30 @@ get '/contacts/:id' do
   end
 end
 
-get '/about' do
-  erb :about
-end
-
-get '/contacts/new' do
-  erb :new_contact
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
